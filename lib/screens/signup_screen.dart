7db8b2759536d01540770f3ea8ee943edf70bcf0
Lalindu Palliyaguruge <151996159+lalindumalaka@users.noreply.github.com>
2025-03-06 17:17:47 +0530

    Update signup_screen.dart

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,3 +1,5 @@
+// veta-app/lib/screens/signup_screen.dart
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // Ensure this import is present
 import 'signin_screen.dart';
