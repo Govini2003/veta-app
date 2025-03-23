3928db15beff28d9b4b7961a82778cbee7fc994e
Lalindu Palliyaguruge <151996159+lalindumalaka@users.noreply.github.com>
2025-03-23 18:07:43 +0530

    Update firebase_options.dart

--- frontend1/lib/Entrance/firebase_options.dart
+++ frontend1/lib/Entrance/firebase_options.dart
@@ -1,3 +1,4 @@
+//veta-app/frontend1/lib/Entrance/firebase_options.dart
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
