0b6775e65eae0e8349b8687f1077952075d5a807
Inupama Asmini <inupamaasmini0724@gmail.com>
2025-03-23 19:30:10 +0530

    Update widget_test.dart

--- frontend1/test/widget_test.dart
+++ frontend1/test/widget_test.dart
@@ -1,5 +1,4 @@
 // This is a basic Flutter widget test.
-//
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
@@ -16,8 +15,8 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('1'), findsOneWidget);//0
+    expect(find.text('0'), findsNothing);//1
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
