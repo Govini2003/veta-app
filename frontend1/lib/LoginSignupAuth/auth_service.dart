b66bae773de6fbf4b0d5c2402deacc5c3daa13d5
Ashen De Zoysa <ashen.20230368@iit.ac.lk>
2025-03-24 01:13:04 +0530

    Update auth_service.dart

--- frontend1/lib/LoginSignupAuth/auth_service.dart
+++ frontend1/lib/LoginSignupAuth/auth_service.dart
@@ -1,4 +1,4 @@
-//frontend1/lib/LoginSignupAuth/auth_service.dart
+//frontend1/lib/LoginSignupAuth/auth_service.dart 
 import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
